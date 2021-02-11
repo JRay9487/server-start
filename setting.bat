7cd36c0099d497cd3fc45b911e4df2058c98f7a0
JRay9487 <65828051+JRay9487@users.noreply.github.com>
2021-02-11 22:16:37 +0800

    Update setting.bat

--- setting.bat
+++ setting.bat
@@ -2,7 +2,7 @@
 //COPYRIGHT FOR JRAY(2021)
 
 rem Do not change unless you know what are you doing
-set MIM_RAM=1G
+set MIN_RAM=1G
 set JAVA_PARAMETERS=-XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:+CMSClassUnloadingEnabled -XX:ParallelGCThreads=5 -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10-XX:PermSize=128m -XX:MaxPermSize=256m -XX:+DisableExplicitGC-XX:+UseConcMarkSweepGC  -XX:+UseNUMA-XX:+CMSParallelRemarkEnabled -XX:MaxGCPauseMillis=50-XX:+UseAdaptiveGCBoundary -XX:-UseGCOverheadLimit -XX:+UseBiasedLocking-XX:SurvivorRatio=8 -XX:TargetSurvivorRatio=90 -XX:MaxTenuringThreshold=15-XX:UseSSE=3 -XX:+UseLargePages -XX:+UseFastAccessorMethods-XX:+UseStringCache -XX:+UseCompressedStrings -XX:+UseCompressedOops-XX:+OptimizeStringConcat -XX:+AggressiveOpts
 
 rem can be changed by user
